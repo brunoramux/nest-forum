c8dacc6336c5fad4b322f2c9912e038f58a263ee
Bruno Ramos Lemos <brunoramoslemos@Brunos-MacBook-Air.local>
2024-01-12 18:10:10 -0300

    Authenticate with JWT in Nest

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { Env } from 'src/env'
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService], // usando um serviço no registro de um modulo. Usamos aqui o ConfigService para pegar variaveis ambiente
+      global: true,
+      useFactory(config: ConfigService<Env, true>) {
+        // "true" informa que nos garantimos a verificacao da tipagem onde pode ser undefined
+        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true }) // pegamos o secret de dentro do env
+        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true }) // pegamos o secret de dentro do env
+
+        return {
+          signOptions: { algorithm: 'RS256' },
+          privateKey: Buffer.from(privateKey, 'base64'),
+          publicKey: Buffer.from(publicKey, 'base64'),
+        }
+      },
+    }),
+  ],
+})
+export class AuthModule {}
--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,8 @@ import { z } from 'zod'
 
 export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
+  JWT_PRIVATE_KEY: z.string(),
+  JWT_PUBLIC_KEY: z.string(),
   PORT: z.coerce.number().optional().default(3333), // coerce tranforma em número pois o dados entra no zod sempre como string
 })
 
