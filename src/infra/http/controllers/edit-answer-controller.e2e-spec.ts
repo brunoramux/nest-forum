29be07573b2b3b90867b718a938836917913a165
Bruno Ramos Lemos <brunoramoslemos@Brunos-MacBook-Air.local>
2024-01-21 18:38:12 -0300

    Edit Answer Controller

---
+++ src/infra/http/controllers/edit-answer-controller.e2e-spec.ts
@@ -0,0 +1,75 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { AnswerFactory } from 'test/factories/make-answer'
+import { QuestionFactory } from 'test/factories/make-question'
+import { StudentFactory } from 'test/factories/make-student'
+
+describe('Edit Question (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let jwt: JwtService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let answerFactory: AnswerFactory
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      // rodamos a aplicação em modo teste
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory, AnswerFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService) // Fazendo com que o PrismaService fique utilizável aqui dentro dos testes para verificarmos se o registro foi gravado no banco de dados.
+    jwt = moduleRef.get(JwtService)
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    answerFactory = moduleRef.get(AnswerFactory)
+
+    await app.init()
+  })
+
+  test('[PUT] /answers/:id', async () => {
+    const user = await studentFactory.makePrismaStudent({
+      name: 'John Doe',
+      email: 'johndoe@teste.com',
+      password: '12345',
+    })
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    const answer = await answerFactory.makePrismaAnswer({
+      questionId: question.id,
+      authorId: user.id,
+    })
+
+    const questionId = question.id.toString()
+
+    const token = jwt.sign({ sub: user.id.toString() })
+
+    const response = await request(app.getHttpServer())
+      .put(`/answers/${answer.id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        content: 'Answer 1',
+      })
+
+    expect(response.statusCode).toBe(204)
+
+    const questionOnDatabase = await prisma.answer.findFirst({
+      where: {
+        content: 'Answer 1',
+      },
+    })
+
+    expect(questionOnDatabase).toBeTruthy()
+  })
+})
---
+++ src/infra/http/controllers/edit-answer.controller.ts
@@ -0,0 +1,47 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Put,
+} from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { TokenSchema } from '@/infra/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { z } from 'zod'
+import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-answer'
+
+const editAnswerBodySchema = z.object({
+  content: z.string(),
+})
+
+type EditAnswerBodySchema = z.infer<typeof editAnswerBodySchema>
+
+@Controller('/answers/:id')
+export class EditAnswerController {
+  constructor(private editAnswer: EditAnswerUseCase) {}
+
+  @Put()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: TokenSchema,
+    @Body(new ZodValidationPipe(editAnswerBodySchema))
+    body: EditAnswerBodySchema,
+    @Param('id') answerId: string,
+  ) {
+    const { content } = body
+    const { sub: userId } = user
+
+    const result = await this.editAnswer.execute({
+      content,
+      authorId: userId,
+      answerId,
+      attachmentsId: [],
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
